081f3dba8c892b7bc6482276d2b3d808ba42f58d
Julia Wuersch <jul.wuersch@gmail.com>
2026-05-05 15:32:16 +0200

    test_minimal_basis not depenedent of sequence of parameters

--- LatticeReduction/Algorithm.py
+++ LatticeReduction/Algorithm.py
@@ -15,14 +15,17 @@ def nearest_integer(q):
   return (q + QQ(1)/2).floor()
 
 def inner_product(v, u, d):
-  return v[0] * u[0] + d * v[1] * u[1]
+  return v[0] * u[0] + abs(d) * v[1] * u[1]
 
 def norm(u, d):
-  return inner_product(u, u, d)  #i.e. u1^2 + d u2^2
+  return inner_product(u, u, d)  #i.e. u1^2 + |d| u2^2
 
 def lattice_reduction_2dim(v, u, d):
+  assert all(isinstance(x, (int, Integer, Rational)) for x in v + u), "v and u should lie in QQ^2"
+
   #Assumption that ||v|| <= ||u||
   if norm(v,d) > norm(u,d):
+    print('Case B: swap')
     v, u = u, v 
 
   #iteration count L
@@ -31,14 +34,15 @@ def lattice_reduction_2dim(v, u, d):
   while norm(v,d) < norm(u,d):
     q = inner_product(u,v,d) / norm(v,d)
     q = nearest_integer(q)
-    r = (u[0] - q * v[0], u[1] - q * v[1])  #u - q * v  faster than using vector(QQ) operations
+    r = (u[0] - q * v[0], u[1] - q * v[1])   #u - q * v  faster than using vector(QQ) operations
     u, v = v, r
     L += 1
   test_minimal_basis(u,v,d)
   return (u,v), L
 
 def test_minimal_basis(v, u, d):
-  assert norm(v,d) <= norm(u,d) and abs(inner_product(v,u,d)) <= norm(v,d)/2, f"The basis ({v}, {u}) is not reduced"
+  assert all(isinstance(x, (int, Integer, Rational)) for x in v + u), "v and u should lie in QQ^2"
+  assert (norm(v,d) <= norm(u,d) and abs(inner_product(v,u,d)) <= norm(v,d)/2) or (norm(u,d) <= norm(v,d) and abs(inner_product(u,v,d)) <= norm(u,d)/2 ), f"The basis ({v}, {u}) is not reduced"
 
 if __name__ == "__main__":
   d = QQ(5)
