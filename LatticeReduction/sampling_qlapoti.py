c2ff0c62f4b279cebce4cf91efa723431e59c1ca
Julia Wuersch <jul.wuersch@gmail.com>
2026-05-15 15:40:11 +0200

    added disk plot

--- LatticeReduction/sampling_qlapoti.py
+++ LatticeReduction/sampling_qlapoti.py
@@ -4,6 +4,7 @@ from quaternion import QuaternionAlgebra
 from euclideanspace import EuclideanSpace
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 def small_generator(n, QA):
   '''I is left ideal, n is the integer n=nrd(I) and b is the basis of I
@@ -86,7 +87,64 @@ def histogram(n, QA, num_samples=1000, log=False):
   plt.savefig(f"histogram_{p_str}_samples_{num_samples}.png")
 
   return histogram
+
+
+def plot_z_in_disk(n, QA, num_samples=1000):
+  E = EuclideanSpace(2)
+  p = QA.p
+  if p == 5 * 2**248 - 1:
+    p_str_title = "5 * 2^248 - 1"
+    p_str = "p1"
+  elif p == 65 * 2**376 - 1:
+    p_str_title = "65 * 2^376 - 1"
+    p_str = "p2"
+  elif p == 27 * 2**500 - 1:
+    p_str_title = "27 * 2^500 - 1"
+    p_str = "p3"
   
+  points = []
+  for _ in range(num_samples):
+    v, u = lattice_vectors(n, QA)
+
+    if E.norm(v) > E.norm(u):
+      v, u = u, v 
+    #z = v/u
+    z = ((v[0]*u[0]+v[1]*u[1])/(u[0]**2+u[1]**2), (v[1]*u[0]-v[0]*u[1])/(u[0]**2+u[1]**2))
+    points.append(z)
+  
+  xs, ys = zip(*points)
+
+  plt.figure(figsize=(6,6))
+
+  
+  # plot the points
+  plt.scatter(xs, ys, s=5, alpha=0.6)
+
+
+  # draw the unit circle
+  theta = np.linspace(0, 2*np.pi, 300)
+  circle_x = np.cos(theta)
+  circle_y = np.sin(theta)
+
+  plt.plot(circle_x, circle_y, 'r', label='|z| = 1')
+  
+  plt.axhline(0)
+  plt.axvline(0)
+  plt.gca().set_aspect('equal', adjustable='box')
+
+
+  plt.title(
+    f"Points z = v/u in the complex plane\n"
+    f"Quaternion algebra B(-1,{p_str_title}), samples = {num_samples}"
+  )
+  plt.xlabel("Re(z)")
+  plt.ylabel("Im(z)")
+  plt.legend()
+
+  plt.savefig(f"Disk_{p_str}_samples_{num_samples}.png")
+
+  plt.show()
+
 
 if __name__ == "__main__":
   p1 = 5 * 2**248 - 1
@@ -98,8 +156,9 @@ if __name__ == "__main__":
   E = EuclideanSpace(2)
   print(f"u: {u}, v: {v}")
 
-  histo = histogram(n, B, num_samples=10000)
-  print(histo)
+  #histo = histogram(n, B, num_samples=10000)
+  #print(histo)
+  plot_z_in_disk(n, B, num_samples=10000)
   
 
   
